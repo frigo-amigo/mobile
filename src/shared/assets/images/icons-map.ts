eae45d1a85ca0d42a4f0ba01ed4c0ae8d8335ada
Lizosoma <101399506+Lizosoma@users.noreply.github.com>
2024-12-22 12:42:19 +0300

    update icons

--- src/shared/assets/images/icons-map.ts
+++ src/shared/assets/images/icons-map.ts
@@ -27,7 +27,6 @@ import ShoppingList from './common/shopping-list.svg';
 import SortGradient from './common/sort-gradient.svg';
 import Sort from './common/sort.svg';
 
-import Apple from './product-icons/apple.svg';
 import Bread from './product-icons/bread.svg';
 import Candies from './product-icons/candies.svg';
 import CannedFood from './product-icons/canned-food.svg';
@@ -39,14 +38,45 @@ import FishAndSeafood from './product-icons/fish-and-seafood.svg';
 import FruitsAndBerries from './product-icons/fruits-and-berries.svg';
 import MeatAndSausages from './product-icons/meat-and-sausages.svg';
 import Nuts from './product-icons/nuts.svg';
-import Pepper from './product-icons/pepper.svg';
 import Undefined from './product-icons/undefined.svg';
-import Potato from './product-icons/potato.svg';
 import ProcessedFood from './product-icons/processed-food.svg';
 import Sauces from './product-icons/sauces.svg';
 import Spices from './product-icons/spices.svg';
-import Tomato from './product-icons/tomato.svg';
 import Vegetables from './product-icons/vegetables.svg';
+
+// Fruits
+import Apple from './product-icons/apple.svg';
+import Apricot from './product-icons/apricot.svg';
+import Banana from './product-icons/banana.svg';
+import Fig from './product-icons/fig.svg';
+import Grape from './product-icons/grape.svg';
+import Mango from './product-icons/mango.svg';
+import Orange from './product-icons/orange.svg';
+import Peach from './product-icons/peach.svg';
+import Pear from './product-icons/pear.svg';
+import Plum from './product-icons/plum.svg';
+
+// Vegetables
+import Broccoli from './product-icons/broccoli.svg';
+import Cabachok from './product-icons/cabachok.svg';
+import Cabbage from './product-icons/cabbage.svg';
+import Carrot from './product-icons/carrot.svg';
+import Caulif from './product-icons/caulif.svg';
+import Celery from './product-icons/celery.svg';
+import Cucumber from './product-icons/cucumber.svg';
+import Eggplant from './product-icons/eggplant.svg';
+import Garlic from './product-icons/garlic.svg';
+import HotPepper from './product-icons/hot-pepper.svg';
+import Onion from './product-icons/onion.svg';
+import PecinCabbage from './product-icons/pecin-cabbage.svg';
+import Pepper from './product-icons/pepper.svg';
+import Potato from './product-icons/potato.svg';
+import Pumpkin from './product-icons/pumpkin.svg';
+import Radish from './product-icons/radish.svg';
+import RedOnion from './product-icons/red-onion.svg';
+import Repa from './product-icons/repa.svg';
+import Svekla from './product-icons/svekla.svg';
+import Tomato from './product-icons/tomato.svg';
 
 export const iconsMap: Record<string, React.FC<SvgProps>> = {
   'add-plus': AddPlus,
@@ -77,28 +107,27 @@ export const iconsMap: Record<string, React.FC<SvgProps>> = {
   'sort-gradient': SortGradient,
   sort: Sort,
 
-  apple: Apple,
-  bread: Bread,
-  candies: Candies,
-  'canned-food': CannedFood,
-  cereal: Cereal,
-  dairy: Dairy,
-  drinks: Drinks,
-  eggs: Eggs,
-  'fish-and-seafood': FishAndSeafood,
-  fruits: FruitsAndBerries,
-  'meat-and-sausages': MeatAndSausages,
-  nuts: Nuts,
-  pepper: Pepper,
-  undefined: Undefined,
-  potato: Potato,
-  'processed-food': ProcessedFood,
-  sauces: Sauces,
-  spices: Spices,
-  tomato: Tomato,
-  vegetables: Vegetables,
+  // apple: Apple,
+  // bread: Bread,
+  // candies: Candies,
+  // 'canned-food': CannedFood,
+  // cereal: Cereal,
+  // dairy: Dairy,
+  // drinks: Drinks,
+  // eggs: Eggs,
+  // 'fish-and-seafood': FishAndSeafood,
+  // fruits: FruitsAndBerries,
+  // 'meat-and-sausages': MeatAndSausages,
+  // nuts: Nuts,
+  // pepper: Pepper,
+  // undefined: Undefined,
+  // potato: Potato,
+  // 'processed-food': ProcessedFood,
+  // sauces: Sauces,
+  // spices: Spices,
+  // tomato: Tomato,
+  // vegetables: Vegetables,
 
-  яблоко: Apple,
   'Хлеб и хлебобулочные изделия': Bread,
   'Сладости и десерты': Candies,
   'Консервированные продукты': CannedFood,
@@ -114,4 +143,36 @@ export const iconsMap: Record<string, React.FC<SvgProps>> = {
   Соусы: Sauces,
   'Специи и приправы': Spices,
   Овощи: Vegetables,
+
+  яблоко: Apple,
+  абрикос: Apricot,
+  банан: Banana,
+  инжир: Fig,
+  виноград: Grape,
+  манго: Mango,
+  апельсин: Orange,
+  персик: Peach,
+  груша: Pear,
+  слива: Plum,
+
+  брокколи: Broccoli,
+  кабачок: Cabachok,
+  капуста: Cabbage,
+  морковь: Carrot,
+  'цветная капуста': Caulif,
+  сельдерей: Celery,
+  огурец: Cucumber,
+  помидор: Tomato,
+  баклажан: Eggplant,
+  чеснок: Garlic,
+  'острый перец': HotPepper,
+  лук: Onion,
+  'пекинская капуста': PecinCabbage,
+  перец: Pepper,
+  картошка: Potato,
+  тыква: Pumpkin,
+  редис: Radish,
+  'красный лук': RedOnion,
+  репа: Repa,
+  свекла: Svekla,
 };
